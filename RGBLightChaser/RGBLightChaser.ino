045988e2163d04a0e80f0d987da1dbbe8578925e
Kekiruu <phamhai2400@gmail.com>
2024-11-03 21:17:46 -0500

    Update RGBLightChaser.ino

--- RGBLightChaser/RGBLightChaser.ino
+++ RGBLightChaser/RGBLightChaser.ino
@@ -1,23 +1,51 @@
 
 void setup() { 
 
-
-  //start serial connection 
-  Serial.begin(9600); 
-
-  //configure pin 3 as an input and enable the internal pull-up resistor 
-  pinMode(3, INPUT_PULLUP); 
+  // initialize digital pin 2,3,4,5 as an output. 
+  pinMode(2, OUTPUT); 
+  pinMode(3, OUTPUT); 
+  pinMode(4, OUTPUT); 
 
 } 
 
-  
+
+// the loop function runs over and over again forever 
 
 void loop() { 
-  //read the pushbutton value into a variable 
-  int sensorVal = !digitalRead(3); 
 
-  //print out the value of the pushbutton 
-  Serial.print("Button State = "); 
-  Serial.println(sensorVal); 
+  digitalWrite(2, HIGH);//turns the RGB red 
+  digitalWrite(3, LOW);  
+  digitalWrite(4, LOW);  
+  delay(500);  // wait for half a second                      
+
+  digitalWrite(2, LOW);//turns the RGB green 
+  digitalWrite(3, HIGH);  
+  digitalWrite(4, LOW); 
+  delay(500);   // wait for half a second     
+
+  digitalWrite(2, LOW);//turns the RGB blue 
+  digitalWrite(3, LOW);  
+  digitalWrite(4, HIGH); 
+  delay(500);   // wait for half a second    
+
+  digitalWrite(2, HIGH);//turns the RGB magenta 
+  digitalWrite(3, LOW);  
+  digitalWrite(4, HIGH); 
+  delay(500);   // wait for half a second    
+
+  digitalWrite(2, HIGH);//turns the RGB yellow 
+  digitalWrite(3, HIGH);  
+  digitalWrite(4, LOW); 
+  delay(500);   // wait for half a second    
+
+  digitalWrite(2, LOW);//turns the RGB cyan 
+  digitalWrite(3, HIGH);  
+  digitalWrite(4, HIGH); 
+  delay(500);   // wait for half a second    
+
+  digitalWrite(2, HIGH);//turns the RGB white 
+  digitalWrite(3, HIGH);  
+  digitalWrite(4, HIGH); 
+  delay(500);   // wait for half a second   
 
 } 
